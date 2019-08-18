8df133892abc6df464954af05f4e5ffbf2898ef8
Vladlen <himaniman@mail.ru>
2019-08-18 11:11:52 +0300

    Try add UT71D

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,15 @@ using System.IO;
 using System.Windows.Threading;
 //using static Xceed.Wpf.Toolkit.DateTimePicker;
 
+//using Device.Net;
+//using Usb.Net.Windows;
+//using Windows.Devices.HumanInterfaceDevice;
+
+using HidSharp.Utility;
+using HidSharp;
+using System.Threading;
+
+
 namespace HP53123_LongExperement
 {
     /// <summary>
@@ -36,12 +45,14 @@ namespace HP53123_LongExperement
         // проблема с перезапуском таймера..
         public SerialPort MainParam_SerialPort;
         public ChartValues<MeasureModel> MainChartValues { get; set; }
+        public Func<double, string> YFormatter { get; set; }
         public int MainParam_CounterMeasure;
         public int MainParam_CounterMeasure_Start;
         public int MainParam_CounterMeasure_End;
         StreamWriter MainParam_StreamWriter;
         DispatcherTimer MainParam_Timer;
         DateTime MainParam_TimeEndMeasure;
+        DateTime MainParam_TimeStartMeasure;
         public void System_ConnectToCOMPortFromAppSettings()
         {
             try
@@ -111,16 +122,38 @@ namespace HP53123_LongExperement
             try
             {
                 string buffer;
-                double value;
+                double value = 0;
                 while (System_serialDataQueue.TryDequeue(out buffer))
                 {
-                    double.TryParse(buffer.Replace(",", "").Replace(".", ",").Replace(" Hz\r", ""), out value);
+                    if (buffer.Contains("kHz"))
+                    {
+                        double.TryParse(buffer.Replace(",", "").Replace(".", ",").Replace(" kHz\r", ""), out value);
+                        value *= 1000L;
+                    }
+                    else if (buffer.Contains("MHz"))
+                    {
+                        double.TryParse(buffer.Replace(",", "").Replace(".", ",").Replace(" MHz\r", ""), out value);
+                        value *= 1000000L;
+                    }
+                    else if (buffer.Contains("GHz"))
+                    {
+                        double.TryParse(buffer.Replace(",", "").Replace(".", ",").Replace(" GHz\r", ""), out value);
+                        value *= 1000000000L;
+                    }
+                    else if (buffer.Contains("Hz"))
+                    {
+                        double.TryParse(buffer.Replace(",", "").Replace(".", ",").Replace(" Hz\r", ""), out value);
+                    }
+
 
                     MainChartValues.Add(new MeasureModel
                     {
                         Label = MainParam_CounterMeasure++,
                         Value = value
                     });
+
+                    if ((bool)RadioButton_FixedPoint.IsChecked) TextBlock_CurrentValue.Text = value.ToString("N") + " Hz";
+                    if ((bool)RadioButton_Scientific.IsChecked) TextBlock_CurrentValue.Text = value.ToString("e") + " Hz";
 
                     while (MainChartValues.Count > 150) MainChartValues.RemoveAt(0);
 
@@ -135,8 +168,28 @@ namespace HP53123_LongExperement
                         if (MainParam_CounterMeasure % int.Parse(TextBox_FragmentSize.Text) == 0)
                         {
                             MainParam_StreamWriter.Flush();
-                            
-                            Label_StatusBurn.Content = "Записан фрагмент " + (MainParam_CounterMeasure / int.Parse(TextBox_FragmentSize.Text)).ToString() + "\nОкончание через " + (MainParam_TimeEndMeasure - DateTime.Now).ToString("dd'.'hh':'mm':'ss");
+                            if (MainParam_CounterMeasure_End == 0)
+                            {
+                                Label_StatusBurn.Content = "Записано значений " + (MainParam_CounterMeasure - MainParam_CounterMeasure_Start).ToString() + "\nОкончание через " + (MainParam_TimeEndMeasure - DateTime.Now).ToString("dd'.'hh':'mm':'ss");
+                            }
+                            else
+                            {
+                                Label_StatusBurn.Content = "Записано значений " + (MainParam_CounterMeasure - MainParam_CounterMeasure_Start).ToString() + "\nОкончание через " + (MainParam_CounterMeasure_End - MainParam_CounterMeasure).ToString();
+                            }
+                        }
+                        if (MainParam_CounterMeasure_End == 0)
+                        {
+                            ProgressBar_Status.Value = ((double)(DateTime.Now.Ticks - MainParam_TimeStartMeasure.Ticks) / (double)(MainParam_TimeEndMeasure.Ticks - MainParam_TimeStartMeasure.Ticks)) * 100;
+                            TextBlock_ETA.Text = "ETA: " + (MainParam_TimeEndMeasure - DateTime.Now).ToString("dd'.'hh':'mm':'ss");
+                        }
+                        if (MainParam_CounterMeasure_End > 0)
+                        {
+                            ProgressBar_Status.Value = ((double)(MainParam_CounterMeasure - MainParam_CounterMeasure_Start) / (double)(MainParam_CounterMeasure_End - MainParam_CounterMeasure_Start)) * 100;
+                            TextBlock_ETA.Text = "ETA: " + (MainParam_CounterMeasure_End - MainParam_CounterMeasure).ToString();
+                            if (MainParam_CounterMeasure_End == MainParam_CounterMeasure)
+                            {
+                                Button_FileBurnStart_Click(null, null);
+                            }
                         }
                     }
                 }
@@ -146,6 +199,10 @@ namespace HP53123_LongExperement
                 MessageBox.Show(ex.ToString());
             }
         }
+
+        HidDevice device;
+        HidStream stream;
+        byte[] buff = new byte[20];
 
         public MainWindow()
         {
@@ -168,7 +225,108 @@ namespace HP53123_LongExperement
 
             TextBox_FilePos.Text = "HP53132_Measure " + DateTime.Now.ToString("dd/MM/yyyy HH-mm-ss") + ".txt";
 
-            DateTimePicker_deadline.Text = "asdsad";
+            YFormatter = value => value.ToString("e");
+
+
+            //Application.Current.Dispatcher.Invoke(myFunc);
+
+            HidSharpDiagnostics.EnableTracing = true;
+            HidSharpDiagnostics.PerformStrictChecks = true;
+            var list = DeviceList.Local;
+            var deviceList = list.GetDevices(DeviceTypes.Hid).ToArray();
+
+            //foreach (Device dev in allDeviceList)
+            //{
+            //    //MessageBox.Show(dev.ToString() + " @ " + dev.DevicePath);
+            //}
+
+            //HidDeviceLoader loader = new HidDeviceLoader();
+            //var deviceList2 = loader.GetDevices().ToArray();
+            String a = "";
+            foreach (HidDevice dev in deviceList)
+            {
+                a += dev + "\n";
+            }
+            MessageBox.Show(a);
+            
+            list.TryGetHidDevice(out device, vendorID: 6790, productID: 57352);
+            if (device == null) MessageBox.Show("error open");
+            
+            if (!device.TryOpen(out stream)) MessageBox.Show("error open stream");
+            //AsyncCallback callBack = new AsyncCallback(myFunc);
+            //IAsyncResult callBack2 = new IAsyncResult(myFunc2);
+
+            byte[] jjj = new byte []{ 0x00, 0x4B, 0x00, 0x00, 0x00 };
+            try
+            {
+                //device.GetRawReportDescriptor();
+                stream.GetFeature(jjj);
+                //stream.SetFeature(jjj);
+            }
+            catch
+            {
+
+            }
+            
+
+            stream.BeginRead(buff, 0, 20, new AsyncCallback(myFunc), null);
+            //stream.EndRead(myFunc2);
+            //while (true)
+            //{
+            //    var bytes = new byte[device.MaxInputReportLength];
+            //    int count;
+
+            //    try
+            //    {
+            //        count = stream.Read(bytes, 0, bytes.Length);
+
+            //    }
+            //    catch (TimeoutException)
+            //    {
+            //        MessageBox.Show("Read timed out.");
+            //        continue;
+            //    }
+            //}
+
+
+        }
+
+        void myFunc(IAsyncResult result)
+        {
+            int bytesRead = 0;
+            try
+            {
+                bytesRead = stream.EndRead(result);
+            }
+            catch
+            {
+
+            }
+
+            //MessageBox.Show(buff.Length.ToString());
+            int a = 0;
+            if (buff[1] == 0xF1) a=((int)buff[2]) + (int)buff[3];
+
+            {
+                Dispatcher.Invoke(
+                    new Action(() =>
+                    {
+                        if (a!=0)
+                        TextBlock_CurrentValue.Text = BitConverter.ToString(buff) + "\n"+ a.ToString();
+                    }
+                    ));
+            }
+
+            //MessageBox.Show(buff.Length.ToString());
+            Thread.Sleep(100);
+
+            buff = new byte[20];
+            stream.BeginRead(buff, 0, 20, new AsyncCallback(myFunc), null);
+        }
+
+        public void myFunc2(IAsyncResult res)
+        {
+
         }
 
         private void ComboBox_COMPorts_DropDownOpened(object sender, EventArgs e)
@@ -265,13 +423,14 @@ namespace HP53123_LongExperement
 
                 RadioButton_FixedPoint.IsEnabled = true;
                 RadioButton_Scientific.IsEnabled = true;
-                RadioButton_Ring.IsEnabled = true;
+                RadioButton_QtyMeas.IsEnabled = true;
                 RadioButton_Timer.IsEnabled = true;
                 TextBox_FilePos.IsEnabled = true;
                 Button_GenerateNewNameFile.IsEnabled = true;
                 Button_SetPathFile.IsEnabled = true;
                 CheckBox_RAWColumn.IsEnabled = true;
                 CheckBox_DateTime.IsEnabled = true;
+                TextBox_FragmentSize.IsEnabled = true;
 
                 MainParam_CounterMeasure_End = MainParam_CounterMeasure;
                 MainChart.AxisX[0].Sections.Add(new AxisSection
@@ -289,6 +448,8 @@ namespace HP53123_LongExperement
                     ". Записано " + (MainParam_CounterMeasure_End - MainParam_CounterMeasure_Start).ToString() + " значений.");
                 MainParam_StreamWriter.Flush();
 
+                ProgressBar_Status.Value = 0;
+                TextBlock_ETA.Text = "";
                 return;
             }
             if (Button_FileBurnStart.Content.ToString() == "Начать запись")
@@ -319,15 +480,17 @@ namespace HP53123_LongExperement
                     Button_FileBurnStart.Background = new SolidColorBrush(Colors.OrangeRed);
                     RadioButton_FixedPoint.IsEnabled = false;
                     RadioButton_Scientific.IsEnabled = false;
-                    RadioButton_Ring.IsEnabled = false;
+                    RadioButton_QtyMeas.IsEnabled = false;
                     RadioButton_Timer.IsEnabled = false;
                     TextBox_FilePos.IsEnabled = false;
                     Button_GenerateNewNameFile.IsEnabled = false;
                     Button_SetPathFile.IsEnabled = false;
                     CheckBox_RAWColumn.IsEnabled = false;
                     CheckBox_DateTime.IsEnabled = false;
+                    TextBox_FragmentSize.IsEnabled = false;
 
                     MainParam_CounterMeasure_Start = MainParam_CounterMeasure;
+                    MainParam_TimeStartMeasure = DateTime.Now;
                     MainChart.AxisX[0].Sections.Add(new AxisSection
                     {
                         Value = MainParam_CounterMeasure_Start,
@@ -338,13 +501,21 @@ namespace HP53123_LongExperement
 
                     Label_StatusBurn.Content = "Запись начата в " + DateTime.Now.ToString("HH:mm");
 
-                    if (MainParam_Timer != null) MainParam_Timer.Stop();
-                     MainParam_Timer = new DispatcherTimer();
+                    if ((bool)RadioButton_Timer.IsChecked)
+                    {
+                        MainParam_CounterMeasure_End = 0;
+                        if (MainParam_Timer != null) MainParam_Timer.Stop();
+                        MainParam_Timer = new DispatcherTimer();
 
-                    MainParam_Timer.Interval = TimeSpanUpDown_Timer.Value.Value;
-                    MainParam_Timer.Start();
-                    MainParam_Timer.Tick += MainParam_Timer_Tick;
-                    MainParam_TimeEndMeasure = DateTime.Now.AddSeconds(TimeSpanUpDown_Timer.Value.Value.TotalSeconds);
+                        MainParam_Timer.Interval = TimeSpanUpDown_Timer.Value.Value;
+                        MainParam_Timer.Start();
+                        MainParam_Timer.Tick += MainParam_Timer_Tick;
+                        MainParam_TimeEndMeasure = DateTime.Now.AddSeconds(TimeSpanUpDown_Timer.Value.Value.TotalSeconds);
+                    }
+                    if ((bool)RadioButton_QtyMeas.IsChecked)
+                    {
+                        MainParam_CounterMeasure_End = MainParam_CounterMeasure + int.Parse(TextBox_QtyMeas.Text);
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -355,18 +526,32 @@ namespace HP53123_LongExperement
 
         private void RadioButton_Timer_Checked(object sender, RoutedEventArgs e)
         {
-            if (DateTimePicker_deadline != null)
+            if (TextBox_QtyMeas != null)
             {
-                if ((bool)RadioButton_Ring.IsChecked)
+                if ((bool)RadioButton_QtyMeas.IsChecked)
                 {
-                    DateTimePicker_deadline.IsEnabled = true;
+                    TextBox_QtyMeas.IsEnabled = true;
                     TimeSpanUpDown_Timer.IsEnabled = false;
                 }
                 if ((bool)RadioButton_Timer.IsChecked)
                 {
-                    DateTimePicker_deadline.IsEnabled = false;
+                    TextBox_QtyMeas.IsEnabled = false;
                     TimeSpanUpDown_Timer.IsEnabled = true;
                 }
+            }
+        }
+
+        private void RadioButton_QtyMeas_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)RadioButton_QtyMeas.IsChecked)
+            {
+                TextBox_QtyMeas.IsEnabled = true;
+                TimeSpanUpDown_Timer.IsEnabled = false;
+            }
+            if ((bool)RadioButton_Timer.IsChecked)
+            {
+                TextBox_QtyMeas.IsEnabled = false;
+                TimeSpanUpDown_Timer.IsEnabled = true;
             }
         }
 
@@ -374,6 +559,21 @@ namespace HP53123_LongExperement
         {
             MainChart.AxisX[0].MinValue = double.NaN;
             MainChart.AxisX[0].MaxValue = double.NaN;
+        }
+
+        private void TextBox_QtyMeas_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Scientific_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)RadioButton_Scientific.IsChecked) YFormatter = value => value.ToString("e");
+            if ((bool)RadioButton_FixedPoint.IsChecked) YFormatter = value => value.ToString("N") + " Hz";
+            MainChart.AxisY[0].LabelFormatter = YFormatter;
+            //Random x = new Random();
+            //System_serialDataQueue.Enqueue("40,"+ x.Next(100,999) + ".100 Hz\r\n");
+            //System_SerialDataProcessing();
         }
     }
 }
